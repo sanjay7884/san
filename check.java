89dcf971cf5164f84dc1488ee2eb55c8b073fc20
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-01-20 21:53:47 +0530

    Create check.java

---
+++ check.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class check
+{
+public static void main (String args[])
+{
+int i;
+Scanner s=new Scanner (System.in);
+i=s.nextInt();
+if(i>0)
+System.out.println("positive");
+else if(i==0)
+System.out.println("zero");
+else 
+System.out.println("negative");
+}
+}
